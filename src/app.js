c0d41807b3a02d4717c3c14e6059a4c950f0d72a
4637@alpha <tyagigaurav345@gmail.com>
2024-06-10 22:08:19 +0530

    final changes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-const cors = require('cors'); 
+const cors = require('cors');
 const express = require('express');
 const compression = require('compression');
+
 const helmet = require('helmet');
 
 const httpStatus = require('http-status');
 const routes = require('./routes');
 const ApiError = require('./utils/ApiError');
 const {errorConverter, errorHandler} = require('./middlewares/error');
+const xss = require('xss-clean');
+
 
 const app = express();
+
+
+app.use(xss());
 
 // set security HTTP headers
 app.use(helmet());
@@ -29,6 +35,9 @@ app.options('*', cors());
 // Reroute all API request starting with "/v1" route
 app.use('/v1', routes);
 
+app.get('/', (req, res) => {
+  res.status(httpStatus.OK).send('Welcome to WanderOn API');
+})
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
